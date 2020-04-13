ed1e21da3b4052ce1e1e2bba0ac9439a99f486fa
sachinm78 <sachinm78@gmail.com>
2020-04-13 11:30:18 -0500

    setting up enigma function

---
+++ index.py
@@ -0,0 +1,15 @@
+print('Project -  Crypto')
+def enigma_light():
+# create keys string
+    keys = 'abcdefghijklmnopqrstuvwxyz !'
+# autogenerate the values string by offsetting original string
+    values = keys[-1] + keys[0:-1]
+    print(keys)
+    print(values)
+# create two dictionaries
+# user input 'the message' and mode
+# run encode or decode
+# return result
+# clean and beautify the code 
+
+enigma_light()
