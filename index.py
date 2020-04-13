442c04ad7af5d4454161a6c2c35ae048723174fe
sachinm78 <sachinm78@gmail.com>
2020-04-13 11:45:59 -0500

    project complete!

--- index.py
+++ index.py
@@ -4,24 +4,24 @@ def enigma_light():
     keys = 'abcdefghijklmnopqrstuvwxyz !'
 # autogenerate the values string by offsetting original string
     values = keys[-1] + keys[0:-1]
-    print(keys)
-    print(values)
+    #print(keys)
+    #print(values)
 # create two dictionaries
     dict_e = dict(zip(keys,values))
     dict_d = dict(zip(values,keys)) 
     # OR create 1 and then flip 
-    # dict_e = dict(zip(keys,values))
-    # dict_d = {value:key for key, value in dict_e.items()}
+    #dict_e = dict(zip(keys,values))
+    #dict_d = {value:key for key, value in dict_e.items()}
 # user input 'the message' and mode
     msg = input('Enter your secret message quietly: ')
-    mode = input('Crypto mode: encode (e) OR decode (d): ')
+    mode = input('Crypto mode: encode (e) OR decrypt as default: ')
 # run encode or decode
-    if mode == 'e':
-        new_msg = [dict_e[letter] for letter in msg]
-    elif mode == 'd':
-        new_msg = [dict_d[letter] for letter in msg]
+    if mode.lower() == 'e':
+        new_msg = ''.join([dict_e[letter] for letter in msg.lower()])
+    else:
+        new_msg = ''.join([dict_d[letter] for letter in msg.lower()])
     
-    return new_msg
+    return new_msg.capitalize()
 # return result
 # clean and beautify the code 
 
